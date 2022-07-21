70a6554b47a8682d01787c26b9d5a369656a88f4
Shyamraj Choudhury <shyamraj.choudhury2@gmail.com>
2022-07-21 21:29:40 +0530

    Updates

---
+++ precache-manifest.afc34debce71ed8da7b4a7421aa1a77f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1a5a2077a2238a6924997cfe179469e",
+    "url": "/keeper_react/index.html"
+  },
+  {
+    "revision": "c4970b253528d8b4f774",
+    "url": "/keeper_react/static/js/2.22d6a103.chunk.js"
+  },
+  {
+    "revision": "02dee7c2558ecee3a184",
+    "url": "/keeper_react/static/js/main.761d2116.chunk.js"
+  },
+  {
+    "revision": "1671b8bd27b1faa5e245",
+    "url": "/keeper_react/static/js/runtime-main.ccbe3196.js"
+  }
+]);
